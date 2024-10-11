7eece5c4590983b580e746a50f6fe8a4ea5434b1
will Farrell <willfarrell@proton.me>
2024-10-11 08:20:04 -0600

    feat: update additionalProperties logic to be more inclusive

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ import { describe } from "node:test";
 import assert from "node:assert";
 
 import jsonSchemaTest from "json-schema-test-esm";
-import Ajv from "ajv";
+import Ajv from "ajv/dist/2020.js";
 import schema from "./index.json" with { type: "json" };
+import exampleSchema from "./example.json" with { type: "json" };
 
 const ajv = new Ajv({
-  schemas: [schema],
+  schemas: [schema, exampleSchema],
   strictTypes: false,
 });
 
